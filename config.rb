d474fb4b92cd51cd1a7033b964355d3657b9b36e
Robert Beekman <robert@matsimitsu.nl>
2017-02-15 21:32:04 +0100

    Add GraphQL docs

--- config.rb
+++ config.rb
@@ -6,6 +6,31 @@ Dotenv.load
 DOCS_ROOT   = File.expand_path(File.dirname(__FILE__))
 GITHUB_ROOT = "https://github.com/appsignal/appsignal-docs/tree/master"
 
+GRAPHQL_OBJECTS = data.graphql.data['__schema']['types']
+  .select  { |t| t['kind'] == 'OBJECT'}
+  .reject  { |t| t['name'] == 'Query' || t['name'].start_with?('__') }
+  .sort_by { |t| t['name'] }
+
+GRAPHQL_INTERFACES = data.graphql.data['__schema']['types']
+  .select  { |t| t['kind'] == 'INTERFACE' }
+  .reject  { |t| t['name'].start_with?('__') }
+  .sort_by { |t| t['name'] }
+
+GRAPHQL_SCALARS = data.graphql.data['__schema']['types']
+  .select  { |t| t['kind'] == 'SCALAR' }
+  .reject  { |t| t['name'].start_with?('__') }
+  .sort_by { |t| t['name'] }
+
+GRAPHQL_ENUMS = data.graphql.data['__schema']['types']
+  .select  { |t| t['kind'] == 'ENUM' }
+  .reject  { |t| t['name'].start_with?('__') }
+  .sort_by { |t| t['name'] }
+
+GRAPHQL_UNIONS = data.graphql.data['__schema']['types']
+  .select  { |t| t['kind'] == 'UNION' }
+  .reject  { |t| t['name'].start_with?('__') }
+  .sort_by { |t| t['name'] }
+
 Time.zone = "Amsterdam"
 
 set :layout, :article
@@ -17,6 +42,27 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 activate :syntax, :line_numbers => true
+
+GRAPHQL_OBJECTS.each do |obj|
+  proxy "/graphql/objects/#{obj['name'].underscore}.html", "/graphql/object.html", :locals => { :obj => obj }, :ignore => true
+end
+
+GRAPHQL_INTERFACES.each do |obj|
+  proxy "/graphql/interfaces/#{obj['name'].underscore}.html", "/graphql/interface.html", :locals => { :obj => obj }, :ignore => true
+end
+
+GRAPHQL_SCALARS.each do |obj|
+  proxy "/graphql/scalars/#{obj['name'].underscore}.html", "/graphql/scalar.html", :locals => { :obj => obj }, :ignore => true
+end
+
+GRAPHQL_ENUMS.each do |obj|
+  proxy "/graphql/enums/#{obj['name'].underscore}.html", "/graphql/enum.html", :locals => { :obj => obj }, :ignore => true
+end
+
+GRAPHQL_UNIONS.each do |obj|
+  proxy "/graphql/unions/#{obj['name'].underscore}.html", "/graphql/union.html", :locals => { :obj => obj }, :ignore => true
+end
+
 
 helpers do
   def link_with_active(name, path)
@@ -31,6 +77,16 @@ helpers do
     page_path = current_page.source_file
     link_to('Create a pull request', page_path.gsub(DOCS_ROOT, GITHUB_ROOT))
   end
+
+  def graphql_query
+    data.graphql.data['__schema']['types'].find { |t| t['name'] == 'Query'}
+  end
+
+  def graphql_objects;    GRAPHQL_OBJECTS;    end
+  def graphql_interfaces; GRAPHQL_INTERFACES; end
+  def graphql_scalars;    GRAPHQL_SCALARS;    end
+  def graphql_enums;      GRAPHQL_ENUMS;      end
+  def graphql_unions;     GRAPHQL_UNIONS;     end
 end
 
 configure :build do
---
+++ source/graphql/index.html.md
@@ -0,0 +1,11 @@
+---
+title: "GraphQL API overview"
+---
+
+## Base endpoint
+
+The base endpoint for all GraphQL API calls is `/graphql` on the `appsignal.com` domain.
+
+```
+https://appsignal.com/graphql
+```
